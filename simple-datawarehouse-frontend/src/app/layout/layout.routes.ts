08bdb9960d2099e1aa6ea5c3cf78c9a86eb61f33
nwolniak <nwolniak7@gmail.com>
2025-06-15 21:08:51 +0200

    simple-datawarehouse-frontend: welcome page

--- simple-datawarehouse-frontend/src/app/layout/layout.routes.ts
+++ simple-datawarehouse-frontend/src/app/layout/layout.routes.ts
@@ -2,8 +2,10 @@ import {Routes} from '@angular/router';
 import {DatabaseComponent} from '@app/database';
 import {EtlComponent} from '@app/etl';
 import {AnalyticsComponent} from "@app/analytics";
+import {WelcomePageComponent} from "@app/layout/welcome-page/welcome-page.component";
 
 export const LAYOUT_ROUTES: Routes = [
+  {path: '', component: WelcomePageComponent},
   {path: 'etl', component: EtlComponent},
   {path: 'database', component: DatabaseComponent},
   {path: 'analytics', component: AnalyticsComponent}
